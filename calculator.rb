4657fd31b3b869a7d8dc56d9377d5e9ea643ca1e
atecdeveloper <atec.developer@gmail.com>
2016-04-05 20:54:06 -0300

    Small programs: Calculator

---
+++ calculator.rb
@@ -0,0 +1,40 @@
+# Calculator program
+operation = ''
+run_calc = 'n'
+
+# Performs the requested calculation
+def calculate(op, x, y)
+  case op
+  when 'plus'
+    x + y
+  when 'minus'
+    x - y
+  when 'divide'
+    x / y
+  when 'multiple'
+    x * y
+  end
+end
+
+while run_calc != "y"
+  puts "---------- Calc v3.0 ----------"
+  puts "Choose first number"
+  num1 = gets.to_f
+  puts "Choose second number"
+  num2 = gets.to_f
+  puts "Choose operation (plus/minus/divide/multiple)"
+  operation = gets.chomp.downcase
+
+  while operation != "plus" && operation != "minus" && operation != "divide" && 
+    operation != "multiple"
+    puts "Operation invalid"
+    puts "Choose operation (plus/minus/divide/multiple)"
+    operation = gets.chomp.downcase
+  end
+
+  puts "-------------------------------"
+  puts "#{num1} #{operation} #{num2} = #{calculate(operation, num1, num2)}"
+  puts "-------------------------------"
+  puts "Do you want to exit? (y/n)"
+  run_calc = gets.chomp.downcase
+end
